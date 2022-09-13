4750a7efcbb24a2cb5ef07a6cfa20b89dc58036a
engrlindagift <lindagift0812@gmail.com>
2022-09-13 08:48:38 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ */
+
+int main (void)
+{
+        int x;
+
+        for (x = 97; x <= 122; x++)
+        {
+                putchar(x);
+        }
+        putchar(10);
+        return (0);
+}
