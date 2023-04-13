904173e6a0c8da72da6f08ab2a60ec1b171100d2
engrlindagift <lindagift0812@gmail.com>
2023-04-13 14:58:38 +0000

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,7 +20,6 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%li] = [%i]\n", i, array[i]);
-		/* check if value at index i equals search value */
 		if (array[i] == value)
 			return (i);
 	}
