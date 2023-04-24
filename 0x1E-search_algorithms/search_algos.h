85a721066bfb37a747eb82378bd8b06c5bcd7b99
engrlindagift <lindagift0812@gmail.com>
2023-04-24 17:32:17 +0000

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -31,5 +31,5 @@ int interpolation(int *array, size_t low, size_t high, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t linear_skip(skiplist_t *list, int value);
 #endif
